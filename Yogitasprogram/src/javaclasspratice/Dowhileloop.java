4992ddbbe451adb90957e815e1d5bb0c294ec46e
77Yogitam <154954315+77Yogitam@users.noreply.github.com>
2025-11-14 13:05:06 +0530

    Push the coad

---
+++ Yogitasprogram/src/javaclasspratice/Dowhileloop.java
@@ -0,0 +1,10 @@
+package javaclasspratice;
+
+public class Dowhileloop {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ Yogitasprogram/src/javaclasspratice/Whilelloop.java
@@ -0,0 +1,58 @@
+package javaclasspratice;
+
+public class Whilelloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// Inncrimental
+	int M=10;
+	while (M<=19)
+	{
+		System.out.println(M);
+		M++;
+		
+	}
+	int Y=100;
+	while( Y<=110)
+	{
+		System.out.println(Y);
+		Y++;
+	}
+	
+	int C=200;
+	while(C>210);
+	{
+		
+		System.out.println(C);
+		C++;
+	}
+	
+	int N=1050;
+	while(N<=1070)
+	{
+		System.out.println(N);
+		N++;
+	}
+	
+	int Spero=90;
+	while(Spero>=100)
+	{
+		System.out.println(Spero);
+		Spero++;
+	}//Decrimental 
+	int java=20;
+	while (java>=10)
+	{
+		System.out.println(java);
+		java--;
+	}
+	
+	//Multiplication
+	
+		
+	
+	
+	}
+
+
+}
