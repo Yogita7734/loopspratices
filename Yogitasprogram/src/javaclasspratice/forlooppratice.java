a2fd3debdf6ad83aee19e164ed094039f243de7b
77Yogitam <154954315+77Yogitam@users.noreply.github.com>
2025-11-14 11:56:53 +0530

    Loops

---
+++ Yogitasprogram/src/javaclasspratice/forlooppratice.java
@@ -0,0 +1,70 @@
+package javaclasspratice;
+
+public class forlooppratice {
+
+	public static void main(String[] args) {
+		// Incrimental operators 
+		for (int yogita=1; yogita<=30;yogita++)
+		{
+			System.out.println(yogita);
+		}
+		
+		for (int Y= 10 ; Y<=20;Y++)
+		{
+			System.out.println(Y);
+		}
+		
+	   for(int N=40;N<=60;N++)
+	   {
+		   System.out.println(N);
+	   }
+	   
+	   for (int C=110; C<=133;C++)
+	   {
+		   System.out.println(C);
+	   }
+	   
+	   for (int P=55; P<=65; P++)
+	   {
+		   System.out.println(P);
+	   }
+	   
+	   for (int Yog=77;Yog<=88;Yog++)
+	   {
+		   System.out.println(Yog);
+	   }
+	   //Decrimental
+	   for (int B=10;B>=5;B--)
+	   {
+		   System.out.println(B);
+	   }
+	   
+	   for (int M=20;M>=10;M--)
+	   {
+		   System.out.println(M);
+	   }
+	   
+	   for (int git=99;git>=88;git--)
+	   {
+		   System.out.println(git);
+		   
+	   }	
+	   for(int java=110; java>=99;java--)
+	   {
+		   System.out.println(java);
+	   }
+	   for (int pra=700;pra>=695;pra--)
+	   {
+		   System.out.println(pra);
+
+		   }
+	   
+	   //Multipluction
+	   int L = 4;
+	   for (int j = 1; j <= 10; j++) {
+	       System.out.println(L + " X " + j + " = " + (L * j));
+	   }
+
+	}
+
+}
